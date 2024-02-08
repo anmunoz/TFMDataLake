c199233760633ce87999f6a53bb0bd53f48d269d
jaimemartinez-99 <jaime.martinez.ramon@alumnos.upm.es>
2024-02-08 13:03:00 +0100

    Changed grammar

--- src/main/scala/org/tfmupm/SparkRead.scala
+++ src/main/scala/org/tfmupm/SparkRead.scala
@@ -6,6 +6,7 @@ import java.io.File
 
 object SparkRead {
   def main(args: Array[String]): Unit = {
+    val startTime = System.nanoTime()
     val spark = SparkSession.builder()
       .master("local[1]")
       .appName("Reading from csv tables")
@@ -15,67 +16,67 @@ object SparkRead {
         "org.apache.spark.sql.delta.catalog.DeltaCatalog")
       .getOrCreate()
 
-    // Define the parent directory
+    // Directorio raíz que se va a analizar para convertir los datos de los sensores en tablas Delta
     val parentDirectory = new File("D:/Archivos uni/TFM/dataset/data/ADL_sequences")
 
-    // Check if the parent directory exists and is a directory
+    // Comprobación de la existencia del directorio raíz
     if (parentDirectory.exists() && parentDirectory.isDirectory) {
-      // List the contents of the parent directory
-      val subdirectories = parentDirectory.listFiles()
+      // Se listan los subdirectorios del directorio raíz, es decir, los pacientes
+      val pacientes = parentDirectory.listFiles()
 
-      // Iterate over each file/subdirectory in the parent directory
-      subdirectories.foreach { subdir =>
-        // Check if it's a directory
-        if (subdir.isDirectory) {
-          val subdirName = subdir.getName
-          println(s"Processing directory: $subdirName")
+      // Se itera sobre cada paciente/subdirectorios
+      pacientes.foreach { paciente =>
+        // Se vuelve a comprobar si es un directorio
+        if (paciente.isDirectory) {
+          val pacienteName = paciente.getName
+          println(s"Procesando paciente: $pacienteName")
 
-          // List the contents of the subdirectory
-          val subsubdirectories = subdir.listFiles()
+          // Se listan los sub-subdirectorios del subdirectorio actual, es decir, las acciones
+          val acciones = paciente.listFiles()
 
-          // Iterate over each sub-subdirectory
-          subsubdirectories.foreach { subsubdir =>
-            // Check if it's a directory
-            if (subsubdir.isDirectory) {
-              val subsubdirName = subsubdir.getName
-              println(s"Processing subdirectory: $subsubdirName")
+          // Se itera sobre cada acción/sub-subdirectorios
+          acciones.foreach { accion =>
+            // Se comprueba que sea un directorio
+            if (accion.isDirectory) {
+              val accionName = accion.getName
+              println(s"Procesando acción: $accionName")
 
-              // Define paths to the CSV files for accelerometer and gyroscope data in the current sub-subdirectory
-              val accelerometerPath = s"${subsubdir.getAbsolutePath}/*-accelerometer-*.csv"
-              val gyroscopePath = s"${subsubdir.getAbsolutePath}/*-gyroscope-*.csv"
+              // Se definen los paths de los archivos CSV de los sensores
+              val accelerometerPath = s"${accion.getAbsolutePath}/*-accelerometer-*.csv"
+              val gyroscopePath = s"${accion.getAbsolutePath}/*-gyroscope-*.csv"
 
-              // Define the base directory where the Delta tables will be saved
+              // Se define el directorio base para las tablas Delta
               val baseDeltaDirectory = "D:/Archivos uni/TFM/TFMDataLake/src/main/scala/org/tfmupm/data"
 
-              // Define paths for Delta tables for accelerometer and gyroscope data in the current sub-subdirectory
-              val subdirectoryDeltaPath = s"$baseDeltaDirectory/$subdirName/$subsubdirName"
-              val accelerometerDeltaPath = s"$subdirectoryDeltaPath/table_accelerometer.delta"
-              val gyroscopeDeltaPath = s"$subdirectoryDeltaPath/table_gyroscope.delta"
+              // Se definen los paths de los directorios de las tablas Delta para el aceletómetro y el giroscopio
+              val pacienteectoryDeltaPath = s"$baseDeltaDirectory/$pacienteName/$accionName"
+              val accelerometerDeltaPath = s"$pacienteectoryDeltaPath/table_accelerometer.delta"
+              val gyroscopeDeltaPath = s"$pacienteectoryDeltaPath/table_gyroscope.delta"
 
-              // Read accelerometer data from CSV and write to Delta table
+              // Se leen los ficheros CSV del acelerómetro y se escriben en las tablas Delta
               val accelerometerDF = spark.read.format("csv").option("header", true).load(accelerometerPath)
               accelerometerDF.write.format("delta").mode("overwrite").save(accelerometerDeltaPath)
 
-              // Read gyroscope data from CSV and write to Delta table
+              // Se leen los ficheros CSV del giroscopio y se escriben en las tablas Delta
               val gyroscopeDF = spark.read.format("csv").option("header", true).load(gyroscopePath)
               gyroscopeDF.write.format("delta").mode("overwrite").save(gyroscopeDeltaPath)
 
-              // Read the Delta tables back for verification or further processing if needed
+              // Se leen las tablas Delta para comprobar su funcionamiento
               val dfAccelerometerTable = spark.read.format("delta").load(accelerometerDeltaPath)
               val dfGyroscopeTable = spark.read.format("delta").load(gyroscopeDeltaPath)
 
-              // Show content of the Delta tables
-              println(s"Accelerometer Table ($subdirName - $subsubdirName):")
+              // Se muestra el contenido de la tabla Delta
+              println(s"Tabla Acelerómetro ($pacienteName - $accionName):")
               dfAccelerometerTable.show()
 
-              println(s"Gyroscope Table ($subdirName - $subsubdirName):")
+              println(s"Tabla Giroscopio ($pacienteName - $accionName):")
               dfGyroscopeTable.show()
 
-              // Print counts of entries in the Delta tables
-              println(s"Accelerometer Table Count ($subdirName - $subsubdirName): " + dfAccelerometerTable.count())
-              println(s"Gyroscope Table Count ($subdirName - $subsubdirName): " + dfGyroscopeTable.count())
+              // Se muestra la cantidad total de entradas de cada tabla Delta.
+              println(s"Entradas en Tabla Acelerómetro ($pacienteName - $accionName): " + dfAccelerometerTable.count())
+              println(s"Entradas en Tabla Giroscopio ($pacienteName - $accionName): " + dfGyroscopeTable.count())
 
-              // Additional checks can be performed using DeltaTable's API if needed
+              //  Información adicional sobre las tablas Delta
               /*
               val deltaTableAccelerometer = DeltaTable.forPath(spark, accelerometerDeltaPath)
               val deltaTableGyroscope = DeltaTable.forPath(spark, gyroscopeDeltaPath)
@@ -89,9 +90,11 @@ object SparkRead {
         }
       }
     } else {
-      println("The specified directory does not exist or is not a directory.")
+      println("El directorio raíz no existe o no es un directorio.")
     }
-
+    val endTime = System.nanoTime()
+    println("Tiempo que tomó el proceso: ")
+    println(((endTime - startTime) / 1000000000) + " segundos")
     spark.stop()
   }
 }
